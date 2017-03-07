21b7acf4d72ef981326e7ae69e463e0722704a22
dungang <dungang@126.com>
2017-03-07 21:05:41 +0800

    php5.4

--- SelectBoxAsset.php
+++ SelectBoxAsset.php
@@ -5,10 +5,14 @@ use yii\web\AssetBundle;
 
 class SelectBoxAsset extends AssetBundle
 {
-    public $sourcePath = __DIR__ . '/assets/select-box';
+   
     public $js = ['select-box.js'];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+    
+    public function init(){
+    	$this->sourcePath = __DIR__ . '/assets/select-box';
+    }
 }
